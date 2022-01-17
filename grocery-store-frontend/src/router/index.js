22a4e4c55106fa1209bd9a4d7807996d4a52f02e
Trung Kien <trungkien7300@gmail.com>
2022-01-17 10:35:07 +0700

    Add routes for router-view

--- grocery-store-frontend/src/router/index.js
+++ grocery-store-frontend/src/router/index.js
@@ -1,7 +1,12 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
-
+import AboutUs from "../views/AboutUs.vue";
+import ContactUs from "../views/ContactUs.vue";
+import PrivacyPolicy from "../views/PrivacyPolicy.vue";
+import TermCondition from "../views/TermCondition.vue";
+import Offer from "../views/Offer.vue";
+import NotFound from "../views/404.vue";
 Vue.use(VueRouter);
 
 const routes = [
@@ -9,6 +14,36 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home,
+  },
+  {
+    path: "/about-us",
+    name: "AboutUs",
+    component: AboutUs,
+  },
+  {
+    path: "/contact-us",
+    name: "ContactUs",
+    component: ContactUs,
+  },
+  {
+    path: "/privacy-policy",
+    name: "PrivacyPolicy",
+    component: PrivacyPolicy,
+  },
+  {
+    path: "/term-condition",
+    name: "TermCondition",
+    component: TermCondition,
+  },
+  {
+    path: "/offer",
+    name: "Offer",
+    component: Offer,
+  },
+  {
+    path: "*",
+    name: "NotFound",
+    component: NotFound,
   },
 ];
 const router = new VueRouter({
