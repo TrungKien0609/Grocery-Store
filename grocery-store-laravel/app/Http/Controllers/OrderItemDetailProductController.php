0e719878e1c43986f7e04e6d19901ede5669d828
Trung Kien <trungkien7300@gmail.com>
2022-02-22 13:52:33 +0700

    create apiresoucre and model with previous migration

---
+++ grocery-store-laravel/app/Http/Controllers/OrderItemDetailProductController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\OrderItemDetailProduct;
+use Illuminate\Http\Request;
+
+class OrderItemDetailProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\OrderItemDetailProduct  $orderItemDetailProduct
+     * @return \Illuminate\Http\Response
+     */
+    public function show(OrderItemDetailProduct $orderItemDetailProduct)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\OrderItemDetailProduct  $orderItemDetailProduct
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, OrderItemDetailProduct $orderItemDetailProduct)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\OrderItemDetailProduct  $orderItemDetailProduct
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(OrderItemDetailProduct $orderItemDetailProduct)
+    {
+        //
+    }
+}
